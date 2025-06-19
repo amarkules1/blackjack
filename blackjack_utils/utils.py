d907e93b3fb87efecb961313d4b03ac3fe941b7a
Alex Markules <amarlar2354@gmail.com>
2025-06-18 23:49:32 -0400

    add utils file

---
+++ blackjack_utils/utils.py
@@ -0,0 +1,38 @@
+from typing import List
+import pandas as pd
+import blackjack_utils.game_config as gc
+import blackjack_utils.shoe as shoe
+import blackjack_utils.card as card
+
+def simulate_hand(game_config: gc.GameConfig, player_starting_cards: List[card.Card], dealer_card_up:card.Card, deck: shoe.Shoe, ev_actions: pd.DataFrame):
+    """
+        uses an EV action table like above to make a decision, then play through the hand
+    """
+    player_total = game_config.score_hand(player_starting_cards)
+    # is_paired = player_starting_cards[0].get_card_value() == player_starting_cards[1].get_card_value()
+    # is_soft = player_starting_cards[0].get_card_value() == 11 or player_starting_cards[1].get_card_value() == 11
+    if player_total > 20:
+        action = 'stand'
+    else:
+        try:
+            action = ev_actions[(ev_actions['player_total'] == str(player_total)) & (ev_actions['dealer_card_up'] == str(dealer_card_up.get_card_value()))]['best_action'].iloc[0]
+        except IndexError:
+            action = 'stand'
+            print(f"player_total: {player_total}, dealer_card_up: {dealer_card_up.get_card_value()}, ev_actions: {ev_actions}")
+    outcome_multiplier = 1
+    can_keep_hitting = True
+    player_cards = player_starting_cards.copy()
+    if action == 'hit':
+        player_cards.append(deck.draw())
+    elif action == 'double':
+        outcome_multiplier = 2
+        can_keep_hitting = False
+        player_cards.append(deck.draw())
+    elif action == 'stand':
+        can_keep_hitting = False
+    player_total = game_config.score_hand(player_cards)
+    if player_total > 20:
+        can_keep_hitting = False
+    if can_keep_hitting:
+        return simulate_hand(game_config, player_cards, dealer_card_up, deck, ev_actions)
+    return outcome_multiplier * game_config.evaluate(player_cards, [dealer_card_up, deck.draw()], deck)
